0141bb59ad6486c4b5b9d38391c7527689c0f6b6
Hooxy <xMolod0yx.Arseny@yandex.ru>
2020-10-19 01:59:26 +0500

    hw

---
+++ sunday/mainapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'mainapp/index.html')
+
+
+def catalog(request):
+    return render(request, 'mainapp/catalog.html')
+
+
+def basket(request):
+    return render(request, 'mainapp/basket.html')
+
+def secret(request):
+    return render(request, 'mainapp/secret.html')
+
